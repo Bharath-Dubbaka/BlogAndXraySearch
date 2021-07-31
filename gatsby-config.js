fb21d8aa389642c5a8aa487007c23a951d4ace5e
Bharath Dubbaka <bharath.reactdev@gmail.com>
2021-07-31 10:46:46 +0530

    added G-tag Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,21 @@ module.exports = {
     name: "nothing",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-RNX9QQMBZ4", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-source-contentful",
       options: {
