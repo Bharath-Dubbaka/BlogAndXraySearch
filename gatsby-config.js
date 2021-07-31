2d1b0d7b7e1a41b86a5f90e1622e446a1478f615
Bharath Dubbaka <bharath.reactdev@gmail.com>
2021-07-31 11:26:37 +0530

    did changes to gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,15 +13,10 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: "gatsby-plugin-google-gtag",
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-XEPFNMSGZJ", // Google Analytics / GA
-        ],
-        // This object is used for configuration specific to this plugin
+        trackingIds: ["G-XEPFNMSGZJ"],
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
           head: true,
         },
       },
